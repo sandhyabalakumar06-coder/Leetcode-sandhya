013adb166e4b746bc866f1cf2d2f0f9497b8bea4
sandhyabalakumar06-coder <sandhyabalakumar06@gmail.com>
2026-08-13 15:44:17 +0530

    Create 1944-TruncateSentence

---
+++ 1944-TruncateSentence/1944-TruncateSentence.java
@@ -0,0 +1,12 @@
+// Last updated: 8/13/2026, 3:44:16 PM
+class Solution {
+  public String truncateSentence(String s, int k) {
+    String[] words = s.split(" ");
+    String[] truncated = new String[k];
+
+    for (int i = 0; i < k; ++i)
+      truncated[i] = words[i];
+
+    return String.join(" ", truncated);
+  }
+}
